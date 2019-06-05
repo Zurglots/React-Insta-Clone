1f8fbbdeabe3bafb02880ff67021909982e6fb0b
Zurglots <bfeole@gmail.com>
2019-06-05 16:16:18 -0400

    PostsPage added appjs refactored

--- instaclone/src/App.js
+++ instaclone/src/App.js
@@ -1,44 +1,17 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.scss";
-import dummyData from "./dummy-data";
-import PostContainer from "./components/PostContainer/PostContainer";
-import SearchBar from "./components/SearchBar/SearchBar";
-
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import {
-  faCompass,
-  faComment,
-  faHeart,
-  faUser
-} from "@fortawesome/free-solid-svg-icons";
-import { far } from "@fortawesome/free-regular-svg-icons";
-
-library.add(fab, far, faCompass, faComment, faHeart, faUser);
+import PostsPage from "./components/PostContainer/PostsPage";
 
 // import CommentSection from "./components/CommentSection/CommentSection";
 
 class App extends React.Component {
-  state = {
-    data: []
-  };
-
-  componentDidMount() {
-    this.setState({
-      data: dummyData
-    });
-  }
+  state = {};
 
   render() {
-    console.log(dummyData);
     return (
       <div className="main-wrapper">
-        <div className="search-header">
-          <SearchBar />
-        </div>
-        <PostContainer data={this.state.data} />
+        <PostsPage />
       </div>
     );
   }
--- instaclone/src/components/CommentSection/CommentSection.js
+++ instaclone/src/components/CommentSection/CommentSection.js
@@ -7,7 +7,8 @@ const randoNames = ["thing1", "thing2", "thing3"];
 class CommentSection extends React.Component {
   state = {
     comments: [],
-    commentInput: ""
+    commentInput: "",
+    randoNames: [{ username: "a guy!", username: "a girl!" }]
   };
 
   //change handler
@@ -15,6 +16,7 @@ class CommentSection extends React.Component {
   componentDidMount() {
     this.setState({
       comments: this.props.coms
+      // randoNames: this.props.coms.username
     });
   }
 
@@ -24,23 +26,26 @@ class CommentSection extends React.Component {
       id: this.state.comments.length + 1,
       username: () => {
         let num = Math.floor(Math.random() * this.state.randoNames.length);
-        return randoNames[num];
+        return this.state.randoNames[num];
       },
       text: this.state.commentInput
     };
     this.setState({
       comments: [...this.state.comments, newComment],
       commentInput: ""
+      // randoNames: [...this.state.randoNames, username()]
     });
   };
 
   handleChanges = e => {
     this.setState({
       commentInput: e.target.value
+      // randomNames: e.target.num
     });
   };
 
   render() {
+    console.log(this.props.coms.username);
     return (
       <div>
         {this.state.comments.map((comment, i) => (
@@ -53,6 +58,7 @@ class CommentSection extends React.Component {
             placeholder="Add a comment..."
             onChange={this.handleChanges}
             value={this.state.commentInput}
+            // num={this.state.randoNames}
             name=""
           />
           <button />
